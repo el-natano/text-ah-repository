c463c9789de21a15c659e611821dc3afa092de42
el-natano <BartonNa816@northwested.org>
2025-03-17 09:24:05 -0400

    h

---
+++ config.py
@@ -0,0 +1,17 @@
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+DARKORANGE = (255, 140, 0)
+
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Window title (caption)
+# Update the window title as needed
+TITLE = "Pygame TEMPLATE 2.0"
+
+# Frame rate (frames per second)
+FPS = 60
